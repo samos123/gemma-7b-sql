2705da90dbf98f7e3c130ae4f5fcef9ac002bd2b
Sam Stoelinga <sammiestoel@gmail.com>
2024-03-16 00:35:41 -0700

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM nvidia/cuda:12.3.2-runtime-ubuntu22.04
+
+RUN apt update -y && apt install -y python3 python3-pip && \
+    rm -rf /var/lib/apt/lists/*
+
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY finetune.py .
+
+CMD python3 finetune.py
---
+++ build-and-run.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -xe
+
+
+export PROJECT_ID=$(gcloud config get project)
+export TAG=${TAG:-latest}
+export IMAGE=gcr.io/${PROJECT_ID}/gemma-finetune:${TAG}
+export BUILD_IMAGE=${BUILD_IMAGE:-yes}
+
+if [ "${BUILD_IMAGE}" = "yes" ]; then
+  docker build --platform linux/amd64 -t ${IMAGE} .
+  docker push ${IMAGE}
+fi
+
+envsubst < job.yaml | kubectl apply -f -
